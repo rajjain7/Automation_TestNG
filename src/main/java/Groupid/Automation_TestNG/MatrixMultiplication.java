aa6385496cfc2e15db512e10cc0a87350a2c567b
Raj Jain <rajjain6201@gmail.com>
2020-01-20 22:07:41 +0530

    program

---
+++ src/main/java/Groupid/Automation_TestNG/MatrixMultiplication.java
@@ -0,0 +1,26 @@
+package Groupid.Automation_TestNG;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+		
+		int[][] a= {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] b= {{1,2,3},{4,5,6},{7,8,9}};
+		
+		int[][] c=new int[3][3];
+		//int[][] k=new int[3][3];
+		
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<b.length;j++) {
+				c[i][j]=0;
+				for(int k=0;k<a.length;k++) {
+					c[i][j]+=a[i][k]*b[k][j];
+				}
+				System.out.println(c[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
