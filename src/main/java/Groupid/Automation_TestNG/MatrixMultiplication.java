321c1d2051533ad7bdba0b7d55a8b771a9c32404
Raj Jain <rajjain6201@gmail.com>
2020-01-20 22:09:57 +0530

    change

--- src/main/java/Groupid/Automation_TestNG/MatrixMultiplication.java
+++ src/main/java/Groupid/Automation_TestNG/MatrixMultiplication.java
@@ -8,7 +8,6 @@ public class MatrixMultiplication {
 		int[][] b= {{1,2,3},{4,5,6},{7,8,9}};
 		
 		int[][] c=new int[3][3];
-		//int[][] k=new int[3][3];
 		
 		for(int i=0;i<a.length;i++) {
 			for(int j=0;j<b.length;j++) {
